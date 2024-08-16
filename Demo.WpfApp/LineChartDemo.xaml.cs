dec60bdf57569dddfc28185de09c98d9be621e26
Nonii Xu <nonii.xu@stardust-inc.com>
2024-08-16 08:15:00 +0800

    demo update

---
+++ Demo.WpfApp/LineChartDemo.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Controls;
+
+namespace Stardust.OpenSource.SolidControls.Demo.WpfApp;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class LineChartDemo : UserControl
+{
+    public LineChartDemo() {
+        InitializeComponent();
+    }
+}
