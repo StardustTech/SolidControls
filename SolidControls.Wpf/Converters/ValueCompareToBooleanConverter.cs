03517ab4daa559d1a15713aad7cd02a6f3e5c307
Nonii Xu <nonii.xu@stardust-inc.com>
2024-08-16 08:14:39 +0800

    v1.1.1 add value compare to boolean converter

---
+++ SolidControls.Wpf/Converters/ValueCompareToBooleanConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Stardust.OpenSource.SolidControls.Wpf
+{
+    public sealed class ValueCompareToBooleanConverter : IValueConverter
+    {
+        private static readonly Lazy<ValueCompareToBooleanConverter> _instance = new Lazy<ValueCompareToBooleanConverter>();
+        public static IValueConverter Instance { get { return _instance.Value; } }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            return value == parameter;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            return (bool)value ? parameter : null;
+        }
+    }
+}
